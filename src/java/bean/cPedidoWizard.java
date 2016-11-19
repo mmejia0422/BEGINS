9b88438d02e10632ae1cf2a304f0c34a27e37378
mmejia0422 <mangelmejia@msn.com>
2016-11-18 19:49:32 -0600

    construyendo la creacion la pedidos:
    Se siguio construyendo el esqueleto del wizard, se cambio el steps por el wizard comoponente de primefaces a la vez se actualizo la libreria de primefaces del 5.4 a la version 6

---
+++ src/java/bean/cPedidoWizard.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.FlowEvent;
+
+/**
+ *
+ * @author Mario
+ */
+@ManagedBean(name="cPedidoWizard")
+@ViewScoped
+public class cPedidoWizard {
+
+    private boolean skip;
+    
+    public cPedidoWizard() {
+    }
+
+    public boolean isSkip() {
+        return skip;
+    }
+
+    public void setSkip(boolean skip) {
+        this.skip = skip;
+    }
+
+     public void save() {        
+        FacesMessage msg = new FacesMessage("Successful", "Welcome");
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
+    
+public String onFlowProcess(FlowEvent event) {
+        if(skip) {
+            skip = false;   //reset in case user goes back
+            return "confirm";
+        }
+        else {
+            return event.getNewStep();
+        }
+    }
+
+
+
+}
